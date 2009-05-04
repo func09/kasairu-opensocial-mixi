2a5224a0620ec05cfc57432ebcae84b975c7b1ef
func09 <mitsuru.haga@gmail.com>
2009-05-04 14:02:25 +0900

    Canvas用のJavaScriptを実装
    リファクタリングでMとVCを分離した

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-var Main = Class.create();
+var Main = createClass();
 Main.prototype = {
   
   /**
@@ -6,7 +6,7 @@ Main.prototype = {
    */
   initialize: function(){
     this.model = new kasairu.Base();
-    this.flashMessage = new gadgets.MiniMessage(__MODULE_ID__,'flash-message');
+    this.flashMessage = new gadgets.MiniMessage(null,'flash-message');
     this.setupViews();
     this.model.getOwnerData($.scope( this.onGetOwnerDataHandler,this));
   }
@@ -23,62 +23,71 @@ Main.prototype = {
     
     // 地域設定変更ボタン押下時の処理
     $('#btn-setting').click($.scope(function(){
+      
       // city_idをセレクトメニューから取得
       this.model.cityId = $('#city-select').attr('value');
+      
       // お天気データを取得
       this.model.fetchWeatherData(this.model.cityId, $.scope(this.onFetchWeatherData, this));
-      // city_idを永続データに保存
-      this.model.updateCityId(this.model.cityId,$.scope(functin(response){
-        if(response.hadError()){
-          this.flashMessage.createTimerMessage("地域設定の保存に失敗しました。もう一度やり直してみてください。", 4 );
-        } else {
-          this.flashMessage.createTimerMessage("地域設定を『"+kasairu.CITIES[this.model.cityId]+"』として保存しました。", 2, function(){
-            $('#box-setting-city').fadeOut();
-            return true;
-          });
-        }
-      },this));
       
-    },this));
+      // city_idを永続データに保存
+      this.model.updateCityId(this.model.cityId, $.scope(this.onUpdateCityId, this));
+      
+    }, this));
+    
   }
   
   /**
    * オーナー情報取得完了後の処理
    */
-  ,onGetOwnerDataHandler: function(response){
+  ,onGetOwnerDataHandler: function(success){
   
-    if (response.hadError()) {
-      return;
-    }
-    
-    this.owner = response.get("get_owner").getData();
-    var ownerAppData = response.get("get_owner_appdata").getData();
-    
     // Owner == Viewerであるか調べる
-    if (this.owner.isViewer()) {
-      this.isAdmin = true;
-    } else {
+    if (!this.model.isAdmin()) {
       $('#box-setting').remove();
     }
     
-    // AppDataからcity_idを取得する
-    if(ownerAppData[owner.getField("id")] && ownerAppData[owner.getField("id")]["city_id"]) {
-      cityId = ownerAppData[owner.getField("id")]["city_id"];
-    } else {
-      cityId = DEFAULT_CITY_ID;
-    }
-
     // セレクトメニューのチェック状態を変更する
-    $('#city-select option[value='+cityId+']').attr('selected','selected');
+    $('#city-select option[value='+this.model.cityId+']').attr('selected','selected');
 
     // お天気情報の取得
-    fetchWeatherData(cityId, $.scope(this.onFetchWeatherData,this));
+    this.model.fetchWeatherData(this.model.cityId, $.scope(this.onFetchWeatherData,this));
   }
   
   /**
    * お天気データの取得完了後の処理
+   * @param isRainy Boolean 雨が振るか？
    */
-  ,onFetchWeatherData: function(data){
+  ,onFetchWeatherData: function(isRainy, success){
+    if (!success) {
+      this.flashMessage.createTimerMessage("お天気情報の取得に失敗しました。しばらくしてからもう一度お試しください。", 4 );
+    } else {
+      // VIEWの変更
+      $(".city-name").text(kasairu.CITIES[this.model.cityId]);
+      if (isRainy) {
+        $("#result-yes").show();
+        $("#result-no").hide();
+      } else {
+        $("#result-yes").hide();
+        $("#result-no").show();
+      }
+    }
+  }
+  
+  /**
+   * city_id保存後の処理
+   */
+  ,onUpdateCityId: function(response){
+    
+    if(response.hadError()){
+      this.flashMessage.createTimerMessage("地域設定の保存に失敗しました。もう一度やり直してみてください。", 4 );
+    } else {
+      this.flashMessage.createTimerMessage("地域設定を『"+kasairu.CITIES[this.model.cityId]+"』として保存しました。", 2, function(){
+        $('#box-setting-city').fadeOut();
+        return true;
+      });
+    }
+    
   }
   
 };
